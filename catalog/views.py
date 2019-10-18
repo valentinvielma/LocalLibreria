d7319803064f67f4fff6777659547c822f245c57
valentinvielma <va.vielma@alumnos.duoc.cl>
2019-10-17 20:08:55 -0400

    otro guardado commit

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+	#Generar contadores de algunos objetos.
+	num_books= Book.objects.all().count()
+	num_instances=BookInstance.objects.all().count()
+
+	#Libros disponibles (estado='a')
+	num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+	num_authors=Author.objects.count() #El 'all()' esta implicito por defecto.
+
+	#renderiza la plantilla html index.html con los datos en las variables contexto
+	return render(
+		request,
+		'index.html',
+		context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+	)
--- locallibreria/settings.py
+++ locallibreria/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'catalog',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'locallibreria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
